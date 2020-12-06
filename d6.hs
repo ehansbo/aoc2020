2c6dba9b5bdf1ba9fbeb2192ac89316ceedd1732
Erik Hansbo <erik.hansbo@seb.se>
2020-12-06 10:30:36 +0100

    Pretty things up 2

--- d6.hs
+++ d6.hs
@@ -1,10 +1,8 @@
-import DayZero
 import Data.List.Split
 import Data.List
 
 main :: IO ()
 main = do
-    groups <- splitFile "input_d6.txt" "\n\n"
-    let q = map (splitOn "\n") groups
+    q <- map (splitOn "\n") <$> splitOn "\n\n" <$> readFile "input_d6.txt"
     print $ sum $ map (length . (foldl1 union)) q
     print $ sum $ map (length . (foldl1 intersect) . filter (/= "")) q
