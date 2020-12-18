c57de5ba121b05ff06a8927ae442e13444b751a9
Erik Hansbo <erik.hansbo@seb.se>
2020-12-18 11:21:58 +0100

    Day 18 cleanup

--- d18.hs
+++ d18.hs
@@ -2,35 +2,31 @@ import DayZero
 import Text.Read
 
 data Expression = Node (Int -> Int -> Int) Expression Expression | Leaf Int
-data Expression2 = Node2 (Int -> Int -> Int) Expression2 Expression2 | Leaf2 Int
 
 instance Show Expression where
     show (Node f e1 e2) = let op = if f 1 1 == 2 then "+" else "*" in "(" ++ op ++ " " ++ show e1 ++ " " ++ show e2 ++ ")"
     show (Leaf i) = show i
 
-instance Show Expression2 where
-    show (Node2 f e1 e2) = let op = if f 1 1 == 2 then "+" else "*" in "(" ++ op ++ " " ++ show e1 ++ " " ++ show e2 ++ ")"
-    show (Leaf2 i) = show i
 
-instance Read Expression where
-    readsPrec _ input = [(parse (filter (/= ' ') $ reverse input), "")]
-        where parse str = 
-                case maybeSplit "*+" str of
-                    Just (str1, op, str2) -> Node (toFunc op) (parse str1) (parse str2)
+parse1 :: String -> Expression
+parse1 input = parse' (filter (/= ' ') $ reverse input)
+    where parse' str =
+            case maybeSplit "*+" str of
+                Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
+                Nothing -> case str of
+                    (')':rest) -> parse' $ reverse $ tail $ reverse rest
+                    (i:[]) -> Leaf $ read [i]
+
+parse2 :: String -> Expression
+parse2 input = parse' (filter (/= ' ') $ reverse input)
+    where parse' str = 
+            case maybeSplit "*" str of
+                Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
+                Nothing -> case maybeSplit "+" str of
+                    Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
                     Nothing -> case str of
-                        (')':rest) -> parse $ reverse $ tail $ reverse rest
+                        (')':rest) -> parse' $ reverse $ tail $ reverse rest
                         (i:[]) -> Leaf $ read [i]
-
-instance Read Expression2 where
-    readsPrec _ input = [(parse (filter (/= ' ') $ reverse input), "")]
-        where parse str = 
-                case maybeSplit "*" str of
-                    Just (str1, op, str2) -> Node2 (toFunc op) (parse str1) (parse str2)
-                    Nothing -> case maybeSplit "+" str of
-                        Just (str1, op, str2) -> Node2 (toFunc op) (parse str1) (parse str2)
-                        Nothing -> case str of
-                            (')':rest) -> parse $ reverse $ tail $ reverse rest
-                            (i:[]) -> Leaf2 $ read [i]
 
 
 maybeSplit :: String -> String -> Maybe (String, Char, String)
@@ -44,20 +40,16 @@ maybeSplit = maybeSplit' "" 0
 
 main :: IO ()
 main = do
-    input <- splitAndReadFile "input_d18.txt" "\n"
-    let tot = sum $ map evaluate input
+    input <- splitFile "input_d18.txt" "\n"
+    let tot = sum $ map (evaluate . parse1) input
     print tot
-    input2 <- splitAndReadFile "input_d18.txt" "\n"
-    print $ sum $ map evaluate2 input2
+    print $ sum $ map (evaluate . parse2) input
 
 
 evaluate :: Expression -> Int
 evaluate (Node op e1 e2) = op (evaluate e1) (evaluate e2)
 evaluate (Leaf i) = i
 
-evaluate2 :: Expression2 -> Int
-evaluate2 (Node2 op e1 e2) = op (evaluate2 e1) (evaluate2 e2)
-evaluate2 (Leaf2 i) = i
 
 toFunc :: Char -> Int -> Int -> Int
 toFunc '+' = (+)
